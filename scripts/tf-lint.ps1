2b951243651b9b6854ee9d1b0f2cfc71cae735a3
Chad <chaddotnet@outlook.com>
2026-09-15 17:04:28 +0700

    remove lint scripts, can run in make

--- scripts/tf-lint.ps1
+++
@@ -1,8 +0,0 @@
-# tflint.ps1
-# For executing in repo root
-# One shared config, disabling root-only rules for module code
-tflint -f compact --recursive --chdir=modules     --config="$PWD/.tflint.modules.hcl"
-tflint -f compact --recursive --chdir=modulegroups --config="$PWD/.tflint.modules.hcl"
-
-# Environments keep normal config discovery (rule stays enabled)
-tflint -f compact --recursive --chdir=environments
