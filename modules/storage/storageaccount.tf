067f45b372d3bb1fe1ba6b3d9c7a9bbb1e06e227
Chad <chaddotnet@outlook.com>
2026-09-22 12:18:51 +0700

    fix validation

--- modules/storage/storageaccount.tf
+++ modules/storage/storageaccount.tf
@@ -38,5 +38,5 @@ resource "azurerm_storage_account" "this" {
   location                 = var.location
   account_tier             = var.account_tier
   account_replication_type = var.replication_type
-
+  tags                     = var.tags
 }
